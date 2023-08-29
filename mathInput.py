4ea8e8c3b0938b47b9e2dc070cb12d42e776e453
IsNotRicardo <desousa.itsnotricardo@gmail.com>
2023-08-29 13:22:01 +0300

    Weight of an object

---
+++ mathInput.py
@@ -0,0 +1,4 @@
+acceleration = float(input("Insert the acceleration due to gravity (m/s^2): "))
+mass = float(input("Insert the mass of an object (kg): "))
+
+print("The weight of the object is:", acceleration * mass, "N")
